7b66f54a88bea50dba4f3f3d1193ba2626e39495
Jerome Zago <jerome.zago@gmail.com>
2017-06-01 00:54:05 +0200

    Using the Video.count() model method

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,6 +13,17 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  console.log('Hello World!')
-  cb();
+  Video.count().exec(function(err, numVideos) {
+    if (err) {
+      return cb(err);
+    }
+
+    if (numVideos > 0) {
+      console.log('Number of video records: ', numVideos);
+    } else {
+      console.log('There are no video records.');
+    }
+
+    return cb();
+  });
 };
